6af10b593993a3d2aa3da1cdaf1117958def3ef4
Fox <foxtrio@proton.me>
2024-07-23 21:27:51 +0300

    Added mobile web vibration functionality

---
+++ source/MobileUtils_gml/extensions/MobileUtils_Web_Vibrate/MobileUtils_Html5_Vibrate.js
@@ -0,0 +1,15 @@
+function MobileUtils_Web_Vibrate_Available() {
+	if (navigator.vibrate) {
+		return 1.0;
+	} else {
+		return 0.0;
+	}
+}
+
+
+function MobileUtils_Web_Vibrate_Shot(milliseconds) {
+
+    navigator.vibrate(milliseconds);
+}
+
+
