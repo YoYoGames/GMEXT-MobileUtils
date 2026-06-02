66f95f2eb15973a2e5324ac341f2b3aa3b2c2706
jzavala-YYG <114691457+jzavala-YYG@users.noreply.github.com>
2026-06-02 04:10:40 +0100

    [Gallery] Return image real name when possible (Android)

--- source/MobileUtils_gml/extensions/MobileUtils_Gallery/AndroidSource/Java/MobileUtils_Gallery.java
+++ source/MobileUtils_gml/extensions/MobileUtils_Gallery/AndroidSource/Java/MobileUtils_Gallery.java
@@ -13,6 +13,10 @@ import java.io.FileOutputStream;
 import java.io.OutputStream;
 import java.io.InputStream;
 
+import android.net.Uri;
+import android.database.Cursor;
+import android.provider.OpenableColumns;
+
 import java.lang.Exception;
 
 public class MobileUtils_Gallery extends RunnerSocial
@@ -31,30 +35,64 @@ public class MobileUtils_Gallery extends RunnerSocial
     }
 	
 	@Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) 
+	public void onActivityResult(int requestCode, int resultCode, Intent data) 
 	{
 		if (resultCode == Activity.RESULT_OK)
 		if (requestCode == PICK_IMAGE_URI) 
 		{
 			try
 			{
-				InputStream input = activity.getContentResolver().openInputStream(data.getData());
-				File file = new File(activity.getFilesDir(),"gallery.jpg");
-				
+				Uri uri = data.getData();
+
+				// Get original file name
+				String fileName = "gallery.jpg"; // fallback
+
+				Cursor cursor = activity.getContentResolver().query(
+						uri,
+						null,
+						null,
+						null,
+						null);
+
+				if (cursor != null)
+				{
+					try
+					{
+						if (cursor.moveToFirst())
+						{
+							int nameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+							if (nameIndex != -1)
+								fileName = cursor.getString(nameIndex);
+						}
+					}
+					finally
+					{
+						cursor.close();
+					}
+				}
+
+				InputStream input = activity.getContentResolver().openInputStream(uri);
+
+				File file = new File(activity.getFilesDir(), fileName);
+
 				OutputStream output = new FileOutputStream(file);
 				byte[] buffer = new byte[4 * 1024];
 				int read;
-				while ((read = input.read(buffer)) != -1) 
+
+				while ((read = input.read(buffer)) != -1)
 					output.write(buffer, 0, read);
+
 				output.flush();
+				output.close();
 				input.close();
-				
+
 				int dsMapIndex = RunnerJNILib.jCreateDsMap(null, null, null);
-				RunnerJNILib.DsMapAddString(dsMapIndex,"type","MobileUtils_Gallery_Open" );
-				RunnerJNILib.DsMapAddString( dsMapIndex,"path",String.valueOf(file));
-				RunnerJNILib.CreateAsynEventWithDSMap(dsMapIndex, EVENT_OTHER_SOCIAL);   
+				RunnerJNILib.DsMapAddString(dsMapIndex, "type", "MobileUtils_Gallery_Open");
+				RunnerJNILib.DsMapAddString(dsMapIndex, "path", file.getAbsolutePath());
+				RunnerJNILib.DsMapAddString(dsMapIndex, "filename", fileName);
+				RunnerJNILib.CreateAsynEventWithDSMap(dsMapIndex, EVENT_OTHER_SOCIAL);
 			}
-			catch (Exception e) 
+			catch (Exception e)
 			{
 				e.printStackTrace();
 			}
--- source/MobileUtils_gml/objects/Obj_MobileUtils_Gallery/Other_70.gml
+++ source/MobileUtils_gml/objects/Obj_MobileUtils_Gallery/Other_70.gml
@@ -2,9 +2,12 @@
 if(async_load[?"type"] == "MobileUtils_Gallery_Open")
 {
 	var _path = async_load[?"path"]
+	show_debug_message($"Path: {_path}")
 	
 	scr_image_tools_square_crop(_path,300)
 		
 	Obj_MobileUtils_Gallery_Picture.sprite = sprite_add(_path,0,0,0,150,150)
+	
+	file_delete(_path)//let's delete the file (most of times we don't need it)
 }
 
